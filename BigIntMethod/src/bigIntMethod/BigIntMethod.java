1cf63f6f2bb8bc8dc4cf5bc17fa2ab49fe5749ae
evanb <evanb@DESKTOP-SQJIV7V>
2018-01-29 08:34:44 -0500

    Evan: third commit, completed testIfPrime method

--- BigIntMethod/src/bigIntMethod/BigIntMethod.java
+++ BigIntMethod/src/bigIntMethod/BigIntMethod.java
@@ -1,5 +1,72 @@
+/***********
+ * Class contains method to test if BigInteger is prime
+ * Bill Nicholson and Evan Batsch
+ * batschew@mail.uc.edu
+ */
+
 package bigIntMethod;
 
+import java.math.BigInteger;
+
+/**
+ * @author Evan Batsch
+ */
 public class BigIntMethod {
+	
+	// Property
+	BigInteger bigInt;
+	
+	// Constructor
+	public BigIntMethod(BigInteger bigInt) {
+		setBigInteger(bigInt);
+	}
+	
+	// Getter
+	public BigInteger getBigInteger() {return bigInt;}
+	// Setter
+	public void setBigInteger(BigInteger bigInt) {this.bigInt = bigInt;}
+	
+	/**
+	 * Tests if a given BigInteger is prime
+	 */
+	// Method
+	public boolean testIfPrime(BigInteger bigInt) {
+		boolean isPrime = true; // flag
+		
+		if (bigInt.intValue() == 2) { // Return true if 2
+			return isPrime;
+		}
+		
+		BigInteger intMax = new BigInteger("2147483647"); // Maximum size of int data type
+		// If BigInteger is less than maximum size of int data type, use cheaper "int" methods
+		if (bigInt.compareTo(intMax) == -1 || bigInt.compareTo(intMax) == 0) {
+			// Integer-based loop
+			for (int i = 2; i < bigInt.intValue(); i++) {
+				if(bigInt.intValue() % i == 0) {
+					isPrime = false;
+					return isPrime;
+				}
+			} return isPrime;
+		// If BigInteger is larger than int maximum, use more expensive methods
+		} else {
+			// Variables to be used in loop below
+			BigInteger one = new BigInteger("1"); 
+			BigInteger zero = new BigInteger("0");
+			BigInteger i = new BigInteger("2");
+			// BigInteger-based loop
+			for (; i.compareTo(bigInt) == -1; i.add(one)) {
+				if ((bigInt.mod(i)).compareTo(zero) == 0) { // if bigInt % i is 0
+					isPrime = false;
+					return isPrime;
+				}
+			} return isPrime; // if bigInt is prime
+		}
+	}
+
+	@Override
+	// To string method
+	public String toString() {
+		return "BigIntMethod [bigInt=" + bigInt + "]";
+	}
 
 }
--- BigIntMethod/src/bigIntMethod/Main.java
+++ BigIntMethod/src/bigIntMethod/Main.java
@@ -1,10 +1,54 @@
+/****************************************
+ * Test main for BigIntMethod class
+ * Bill Nicholson and Evan Batsch
+ * nicholdw@ucmail.uc.edu
+ */
+
 package bigIntMethod;
 
+import java.math.BigInteger;
+
+/**
+ * @author Bill Nicholson
+ */
 public class Main {
 
 	public static void main(String[] args) {
-		
+
+		TestCase testCases[] = {
+							new TestCase("2", false),
+				            new TestCase("3", true),
+				            new TestCase("4", false),
+				          	new TestCase("7", true),
+				            new TestCase("97",true),
+				            new TestCase("100",false),
+				            new TestCase("101",false),
+				            new TestCase("2038074743", false), // takes ~10 seconds
+				            //new TestCase("5112733757", false) takes a few minutes
+		};
+		int testsPassed = 0, testsFailed = 0, testNumber = 0;
+		BigIntMethod bigIntMethod = new BigIntMethod(new BigInteger("0"));
+		for (TestCase tc : testCases) {
+			testNumber++;
+			boolean result = bigIntMethod.testIfPrime(new BigInteger(tc.test));
+			if (result ==  tc.expectedResult) {
+				testsPassed++;
+			} else {
+				testsFailed++;
+				System.out.println("Test " + testNumber + " failed (" + tc.test + ") .");
+			}
+		}
+		if (testsPassed == testCases.length) {
+			System.out.println("All tests passed.");
+		} else {
+			System.out.println(testCases.length - testNumber + " tests failed.");
+		}
+		System.out.println();
 
 	}
-
+	private static class TestCase {
+		public TestCase(String test, boolean expectedResult) {this.test = test; this.expectedResult = expectedResult;}
+		public String test;
+		public boolean expectedResult;
+	}
 }
